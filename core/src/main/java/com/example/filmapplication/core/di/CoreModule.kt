fb5d9ea09f455ed44a7f700dd3e87a964150dca3
srpradana <danapradana11@gmail.com>
2021-06-11 18:28:44 +0700

    modified security

--- core/src/main/java/com/example/filmapplication/core/di/CoreModule.kt
+++ core/src/main/java/com/example/filmapplication/core/di/CoreModule.kt
@@ -8,6 +8,9 @@ import com.example.filmapplication.core.data.source.remote.RemoteDataSource
 import com.example.filmapplication.core.domain.repository.IMoviesRepository
 import com.example.filmapplication.core.data.source.remote.network.ApiService
 import com.example.filmapplication.core.utils.AppExecutors
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -19,19 +22,26 @@ import java.util.concurrent.TimeUnit
 val databaseModule = module {
     factory { get<FilmDatabase>().filmDao() }
     single {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("crypto".toCharArray())
+        val factory = SupportFactory(passphrase)
         Room.databaseBuilder(
             androidContext(),
             FilmDatabase::class.java, "Film.db"
-        ).fallbackToDestructiveMigration().build()
+        ).fallbackToDestructiveMigration().openHelperFactory(factory).build()
     }
 }
 
 val networkModule = module {
     single {
+        val hostname = "api.themoviedb.org"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/+vqZVAzTqUP8BGkfl88yU7SQ3C8J2uNEa55B7RZjEg0=")
+            .build()
         OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
     single {
